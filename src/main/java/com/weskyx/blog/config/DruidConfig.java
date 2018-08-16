3cf1c2bddbd7580f17ca485ae97ed7ff8d608727
neverxp <1053334228@qq.com>
2018-08-17 00:49:24 +0800

    配置mysql连接池；修正错误

---
+++ src/main/java/com/weskyx/blog/config/DruidConfig.java
@@ -0,0 +1,135 @@
+package com.weskyx.blog.config;
+
+import com.alibaba.druid.filter.Filter;
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
+import com.alibaba.druid.wall.WallConfig;
+import com.alibaba.druid.wall.WallFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+public class DruidConfig {
+    @Value("${spring.datasource.url}")
+    private String url;
+    @Value("${spring.datasource.username}")
+    private String username;
+    @Value("${spring.datasource.password}")
+    private String password;
+    @Value("${spring.datasource.driver-class-name}")
+    private String driverClassName;
+
+    @Value("${spring.datasource.initialSize}")
+    private int initialSize;
+    @Value("${spring.datasource.minIdle}")
+    private int minIdle;
+    @Value("${spring.datasource.maxActive}")
+    private int maxActive;
+    @Value("${spring.datasource.maxWait}")
+    private int maxWait;
+    @Value("${spring.datasource.timeBetweenEvictionRunsMillis}")
+    private int timeBetweenEvictionRunsMillis;
+    @Value("${spring.datasource.minEvictableIdleTimeMillis}")
+    private int minEvictableIdleTimeMillis;
+    @Value("${spring.datasource.validationQuery}")
+    private String validationQuery;
+    @Value("${spring.datasource.testWhileIdle}")
+    private boolean testWhileIdle;
+    @Value("${spring.datasource.testOnBorrow}")
+    private boolean testOnBorrow;
+    @Value("${spring.datasource.testOnReturn}")
+    private boolean testOnReturn;
+    @Value("${spring.datasource.poolPreparedStatements}")
+    private boolean poolPreparedStatements;
+    @Value("${spring.datasource.maxPoolPreparedStatementPerConnectionSize}")
+    private int maxPoolPreparedStatementPerConnectionSize;
+    @Value("${spring.datasource.filters}")
+    private String filters;
+    @Value("${spring.datasource.logSlowSql}")
+    private String logSlowSql;
+
+    @Autowired
+    WallFilter wallFilter;
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertyPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+
+    @Bean
+    public ServletRegistrationBean druidServlet() {
+        ServletRegistrationBean regBean = new ServletRegistrationBean();
+        regBean.setServlet(new StatViewServlet());
+        regBean.addUrlMappings("/druid/*");
+        regBean.addInitParameter("loginUsername", username);
+        regBean.addInitParameter("loginPassword", password);
+        regBean.addInitParameter("logSlowSql", logSlowSql);
+        return regBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean filterRegistrationBean() {
+        FilterRegistrationBean filterBean = new FilterRegistrationBean();
+        filterBean.setFilter(new WebStatFilter());
+        filterBean.addUrlPatterns("/*");
+        filterBean.addInitParameter("exclusions", "*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid/*");
+        filterBean.addInitParameter("profileEnable", "true");
+        return filterBean;
+    }
+
+    @Bean(name = "wallFilter")
+    @DependsOn("wallConfig")
+    public WallFilter wallFilter(WallConfig wallConfig) {
+        WallFilter wallFilter = new WallFilter();
+        wallFilter.setConfig(wallConfig);
+        return wallFilter;
+    }
+
+    @Bean(name = "wallConfig")
+    public WallConfig wallConfig() {
+        WallConfig wallConfig = new WallConfig();
+        wallConfig.setMultiStatementAllow(true);
+        return wallConfig;
+    }
+
+
+    @Bean
+    public DataSource druidDataSource() {
+        DruidDataSource dataSource = new DruidDataSource();
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        dataSource.setDriverClassName(driverClassName);
+        dataSource.setInitialSize(initialSize);
+        dataSource.setMinIdle(minIdle);
+        dataSource.setMaxActive(maxActive);
+        dataSource.setMaxWait(maxWait);
+        dataSource.setTimeBetweenEvictionRunsMillis(timeBetweenEvictionRunsMillis);
+        dataSource.setMinEvictableIdleTimeMillis(minEvictableIdleTimeMillis);
+        dataSource.setValidationQuery(validationQuery);
+        dataSource.setTestWhileIdle(testWhileIdle);
+        dataSource.setTestOnBorrow(testOnBorrow);
+        dataSource.setTestOnReturn(testOnReturn);
+        try {
+            dataSource.setFilters(filters);
+            List<Filter> filters = new ArrayList<>();
+            filters.add(wallFilter);
+            dataSource.setProxyFilters(filters);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return dataSource;
+    }
+}
--- src/main/java/com/weskyx/blog/service/impl/ArticleServiceImpl.java
+++ src/main/java/com/weskyx/blog/service/impl/ArticleServiceImpl.java
@@ -1,12 +1,8 @@
 package com.weskyx.blog.service.impl;
 
-import com.weskyx.blog.common.ResultBuilder;
-import com.weskyx.blog.repository.IImageRepository;
 import com.weskyx.blog.service.IArticleService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.multipart.MultipartFile;
 
 @Service
 @Transactional
